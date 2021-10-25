715cb388fbbba115f9b6683aa0f171f65e2f0af0
Aliya Miranda <miranda.aliya@gmail.com>
2021-10-25 23:04:08 +0800

    landing page more or less the same except for a few paddings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
